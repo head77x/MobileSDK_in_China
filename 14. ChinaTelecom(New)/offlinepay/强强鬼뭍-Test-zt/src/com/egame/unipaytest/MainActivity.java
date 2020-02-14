985a7316c06ce1eccdec4f034bf6d91f7398820a
Brandon Chung <cto@netmego.com>
2020-02-14 17:05:23 +0900

    차이나텔레콤 신규 SDK

---
+++ 14. ChinaTelecom(New)/offlinepay/强强鬼뭍-Test-zt/src/com/egame/unipaytest/MainActivity.java
@@ -0,0 +1,116 @@
+package com.egame.unipaytest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import android.app.Activity;
+import android.app.AlertDialog.Builder;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import cn.egame.terminal.paysdk.EgameExitListener;
+import cn.egame.terminal.paysdk.EgamePay;
+import cn.egame.terminal.paysdk.EgamePayListener;
+
+
+
+
+public class MainActivity extends Activity {
+	Activity thisActivity;
+	Button button1,button2,button3,button4;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		thisActivity=this;
+		EgamePay.init(this);
+		
+		button1=(Button) findViewById(R.id.button1);
+		button2=(Button) findViewById(R.id.button2);
+		button3=(Button) findViewById(R.id.button3);
+		button4=(Button) findViewById(R.id.button4);
+		
+		button1.setOnClickListener(button1_onClickListener);
+		button2.setOnClickListener(button2_onClickListener);
+		button3.setOnClickListener(button3_onClickListener);
+		button4.setOnClickListener(button4_onClickListener);
+	}
+	
+	
+
+	  private OnClickListener button3_onClickListener=new OnClickListener(){
+	    	public void onClick(View arg0) {  
+	    		
+	        EgamePay.moreGame(thisActivity);//更多游戏接口
+	        
+	        }  
+	    };
+	    
+	    private OnClickListener button4_onClickListener=new OnClickListener(){
+	    	public void onClick(View arg0) {  
+	    		EgamePay.exit(thisActivity, new EgameExitListener() { //Main.this为主Activity
+
+	    			public void exit() {
+	    				finish();
+	   
+	    				//退出游戏操作
+	    				
+	    			}
+
+	    			public void cancel() {
+	    				//取消退出，返回游戏
+	    				
+	    			}
+	    		});	  
+	        }  
+	    };
+	
+	private OnClickListener button1_onClickListener=new OnClickListener() {
+		@Override
+		public void onClick(View arg0) {
+			HashMap<String, String> payParams=new HashMap<String, String>();
+			payParams.put(EgamePay.PAY_PARAMS_KEY_TOOLS_ALIAS, "5245217");
+			payParams.put(EgamePay.PAY_PARAMS_KEY_PRIORITY, "sms");
+			Pay(payParams);
+		}
+	};
+	
+	private OnClickListener button2_onClickListener=new OnClickListener() {
+		@Override
+		public void onClick(View arg0) {
+			HashMap<String, String> payParams=new HashMap<String, String>();
+			payParams.put(EgamePay.PAY_PARAMS_KEY_TOOLS_ALIAS, "5245218");
+		    payParams.put(EgamePay.PAY_PARAMS_KEY_PRIORITY, "sms");
+			Pay(payParams);
+		}
+	};
+	
+
+	private void Pay(HashMap<String, String> payParams){
+		final Builder dialog=new Builder(thisActivity);
+		dialog.setTitle("支付SDK测试");
+		
+		EgamePay.pay(thisActivity, payParams,new EgamePayListener() {
+			@Override
+			public void paySuccess(Map<String, String> params) {
+				dialog.setMessage("道具"+params.get(EgamePay.PAY_PARAMS_KEY_TOOLS_ALIAS)+"支付成功");
+				dialog.show();
+			}
+			
+			@Override
+			public void payFailed(Map<String, String> params, int errorInt) {
+				dialog.setMessage("道具"+params.get(EgamePay.PAY_PARAMS_KEY_TOOLS_ALIAS)+"支付失败：错误代码："+errorInt);
+				dialog.show();
+			}
+			
+			@Override
+			public void payCancel(Map<String, String> params) {
+				dialog.setMessage("道具"+params.get(EgamePay.PAY_PARAMS_KEY_TOOLS_ALIAS)+"支付已取消");
+				dialog.show();
+			}
+		});
+	}
+
+}
